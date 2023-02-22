5191ee523c0263e5c8d89c2b24b4ec912588f6d7
mbeaver502 <jmbeaver1@gmail.com>
2023-02-21 21:35:04 -0600

    Create a basic template for the home page

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -23,7 +26,21 @@ func main() {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello, World!</h1>")
+
+	tpl, err := template.ParseFiles(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		// Note: the status code won't get updated if data has already been written to `w` (i.e., by `tpl.Execute()`)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
