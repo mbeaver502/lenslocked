1c0fc0f5ca180057cd0fb9ae2b8745d86ba04322
mbeaver502 <jmbeaver1@gmail.com>
2023-02-21 20:14:04 -0600

    Use chi middleware and URL params

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/mbeaver502/lenslocked
 go 1.20
 
 require github.com/go-chi/chi/v5 v5.0.8
+
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
 
+	//r.Use(middleware.Logger)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	//r.Get("/test/{param}", urlParamHandler)
+	r.With(middleware.Logger).Get("/test/{param}", urlParamHandler)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
@@ -35,4 +40,9 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1><p>Frequently asked questions appear here...</p>")
+}
+
+func urlParamHandler(w http.ResponseWriter, r *http.Request) {
+	param := chi.URLParam(r, "param")
+	fmt.Fprint(w, param)
 }
