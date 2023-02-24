1b7a1c414d367b48c48256179cad536c5fe307e5
mbeaver502 <jmbeaver1@gmail.com>
2023-02-23 21:35:10 -0600

    Use URL query param to prepopulate email on signup

--- controllers/users.go
+++ controllers/users.go
@@ -12,7 +12,14 @@ type Users struct {
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	u.Templates.New.Execute(w, nil)
+	var data struct {
+		Email string
+	}
+
+	// Get the value from the `email` URL query param
+	data.Email = r.FormValue("email")
+
+	u.Templates.New.Execute(w, data)
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
