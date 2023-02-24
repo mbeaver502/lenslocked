ce493db1cecb47a71d7795f4700787e5aa8d9d40
mbeaver502 <jmbeaver1@gmail.com>
2023-02-23 21:23:34 -0600

    Get POST form values in Users.Create

--- controllers/users.go
+++ controllers/users.go
@@ -16,5 +16,20 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello from Create!")
+	// err := r.ParseForm()
+	// if err != nil {
+	// 	http.Error(w, "Failed to parse form", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// email := r.PostForm.Get("email")
+	// password := r.PostForm.Get("password")
+
+	// We can use PostFormValue to automatically parse the form
+	// before retrieving a value (if it exists), but we ignore
+	// any parse errors that occur.
+	email := r.PostFormValue("email")
+	password := r.PostFormValue("password")
+
+	fmt.Fprint(w, email, password)
 }
