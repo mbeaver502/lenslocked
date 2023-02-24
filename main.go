ff703dbafc0ecd73e03b7d1317c8b8c5097df115
mbeaver502 <jmbeaver1@gmail.com>
2023-02-23 21:01:58 -0600

    Create a Users controller

--- main.go
+++ main.go
@@ -28,10 +28,9 @@ func main() {
 			views.Must(
 				views.ParseFS(templates.FS, "tailwind.gohtml", "faq.gohtml"))))
 
-	r.Get("/signup",
-		controllers.StaticHandler(
-			views.Must(
-				views.ParseFS(templates.FS, "tailwind.gohtml", "signup.gohtml"))))
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "tailwind.gohtml", "signup.gohtml"))
+	r.Get("/signup", usersC.New)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
