da6e93ba32056052c706abb7d8590fda5c3d0d64
mbeaver502 <jmbeaver1@gmail.com>
2023-03-05 18:26:20 -0600

    Stub funcs for SessionService

--- models/session.go
+++ models/session.go
@@ -1,7 +1,30 @@
 package models
 
+import (
+	"database/sql"
+)
+
 type Session struct {
-	ID        uint
-	UserID    uint
+	ID     uint
+	UserID uint
+	// Token is only set when creating a new session.
+	// When looking up a session, this will be left empty,
+	// as we only store the hash of a session token in the DB
+	// and we cannot reverse it into a raw token.
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID uint) (*Session, error) {
+	// TODO: Create the session token
+	// TODO: Implement SessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	return nil, nil
 }
