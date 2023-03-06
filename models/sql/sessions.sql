d95290f1d937621c87dd3986ff2e81c7215796e4
mbeaver502 <jmbeaver1@gmail.com>
2023-03-05 21:01:30 -0600

    Add foreign key to sessions table

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -1,5 +1,9 @@
 create table sessions (
     id serial primary key,
     user_id int unique not null,
-    token_hash text unique not null
+    token_hash text unique not null,
+
+    foreign key (user_id) references users (id)
 );
+
+-- alter table sessions add constraint fk_sessions_user_id foreign key (user_id) references users (id);
