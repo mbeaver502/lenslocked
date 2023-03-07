95dde91bee685104ae41bdf8c56b5f723c1ea87c
mbeaver502 <jmbeaver1@gmail.com>
2023-03-06 18:27:31 -0600

    Remove old SQL files

--- models/sql/sessions.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE sessions (
-    id serial PRIMARY KEY,
-    user_id int UNIQUE NOT NULL,
-    token_hash text UNIQUE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
--- alter table sessions add constraint fk_sessions_user_id foreign key (user_id) references users (id);
--- models/sql/users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id serial PRIMARY KEY,
-    email text UNIQUE NOT NULL,
-    password_hash text NOT NULL
-);
