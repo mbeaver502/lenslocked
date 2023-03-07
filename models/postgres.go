0328ecc42ac72aa79807fcfbbd2ded76d4b36fb1
mbeaver502 <jmbeaver1@gmail.com>
2023-03-06 18:09:47 -0600

    Use goose to run migrations in Go

--- models/postgres.go
+++ models/postgres.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 type PostgresConfig struct {
@@ -48,4 +49,18 @@ func (cfg PostgresConfig) String() string {
 		cfg.Password,
 		cfg.DBName,
 		cfg.SSLMode)
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("migrate: %w", err)
+	}
+
+	err = goose.Up(db, dir)
+	if err != nil {
+		return fmt.Errorf("migrate: %w", err)
+	}
+
+	return nil
 }
